d2e1af4f78cf57e6252186e4c8c242825f6004f3
Lincoln Nowell <lincolnnowell@gmail.com>
2019-03-16 14:55:08 -0500

    Add files via upload

---
+++ Master Git Updater.bat
@@ -0,0 +1,34 @@
+cd "Visual Studio Projects"
+git status
+git add .
+git commit -am "Visual Studio Projects"
+git status
+
+cd ..
+cd "MyWebsiteV2"
+git status
+git add .
+git commit -am "MyWebsiteV2"
+git status
+
+cd ..
+cd "VS Code Files"
+git status
+git add .
+git commit -am "VS Code Files"
+git status
+
+cd ..
+cd "Java Files"
+git status
+git add .
+git commit -am "Java Files"
+git status
+
+cd.. 
+cd "Qt files"
+git status
+git add .
+git commit -am "Qt files"
+git status
+pause
